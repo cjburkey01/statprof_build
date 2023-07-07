0825e2646911ad40b85160025ece57c57236169a
CJ Burkey <cjburkey01@gmail.com>
2023-07-06 20:57:43 -0400

    Make them all work

---
+++ build.sh
@@ -0,0 +1,14 @@
+echo "Delete build dir"
+rm -rf build
+
+echo "Re-create empty build dir"
+mkdir -p build
+
+echo "Run the tera-cli crate (with backtrace)"
+# Inserts the output into build/index.html
+RUST_BACKTRACE=1 tera --template templates/index.tera --include empty.json > build/index.html
+
+echo "Copy files from public dir to build dir"
+cp public/* build/
+
+echo "Done!"
