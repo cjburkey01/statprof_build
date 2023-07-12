e3a4021c163ea57e29badbbfcc096b2fd653fe14
CJ Burkey <cjburkey01@gmail.com>
2023-07-12 05:13:14 -0400

    Start working on some minor refactors to make life easier

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ mkdir -p build
 
 echo "Run the tera-cli crate (with backtrace)"
 # Inserts the output into build/index.html
-RUST_BACKTRACE=1 tera --template templates/index.tera --include empty.json > build/index.html
+RUST_BACKTRACE=1 tera --template templates/index2.tera --include empty.json > build/index.html
 
 echo "Copy files from public dir to build dir"
 cp public/* build/
